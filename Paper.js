7d32f1eedfbbc55a39e9b2b880d10dd1480ed2ed
NikhilJacob2007 <79143357+NikhilJacob2007@users.noreply.github.com>
2021-05-10 13:19:45 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,42 @@
+class Paper {
+
+    constructor(x, y, radius){
+
+        var options = {
+
+            isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:1.2
+
+        }
+
+        this.x = x;
+        this.y = y;
+
+        this.r = radius;
+
+        this.image = loadImage("paper.png");
+
+        this.body = Bodies.circle(x, y, this.r, options);
+
+        World.add(world, this.body);
+
+    }
+
+    display() {
+
+        var pos = this.body.position;
+
+        push()
+
+        translate(pos.x, pos.y);
+        imageMode(CENTER);
+        //ellipse(0, 0, this.r);
+        image(this.image, 0, this.r);
+
+        pop()
+
+    }
+
+}
